1fd66cc98dc87492a18d85730e4d98629959bacc
Leonardo Margiotta <leonardomargiotta@Leonardos-MacBook-Pro.local>
2025-11-09 15:42:38 +0100

    filters in esempi

--- client/src/pages/Esempi.tsx
+++ client/src/pages/Esempi.tsx
@@ -7,7 +7,7 @@ import Header from "@/components/header";
 import Footer from "@/components/footer";
 import comingPath from "@assets/coming_soon.webp"
 
-const categories = ["tutti", "ristoranti", "dentisti", "palestre", "centri estetici", "sium"];
+const categories = ["tutti", "ristoranti", "dentisti", "palestre", "hotel"];
 
 // Dati degli esempi di siti
 const websiteExamples = [
