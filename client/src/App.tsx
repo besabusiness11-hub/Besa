e3a5568b56cbc86da82fc456d5090f69307fc08a
Leonardo Margiotta <leonardomargiotta@Leonardos-MacBook-Pro.local>
2025-12-02 10:56:28 +0100

    spash screen load

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import { Route, Switch } from "wouter";
 import Home from "./pages/home";
 import Esempi from "./pages/Esempi";
 import Categories from "./pages/categories";
 import NotFound from "./pages/not-found";
+import { SplashScreen } from "./components/splash-screen";
 
 function App() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  if (showSplash) {
+    return <SplashScreen onComplete={() => setShowSplash(false)} />;
+  }
+
   return (
     <Switch>
       <Route path="/" component={Home} />
